d5cc1781257d8fb02149f72d75e5391cc4c6523e
Sarra-dev <srrbettaieb@gmail.com>
2025-11-14 14:30:43 +0100

    update

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,27 +1,35 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyFirstApp.Models;
 
-
-namespace MyFirstApp.Controllers;
-
-
-
-public class MovieController : Controller
+namespace MyFirstApp.Controllers
 {
-    public MovieController()
+    public class MovieController : Controller
     {
-        
-    }
-    public IActionResult Index()
-    {
-        var movies = new List<Movie>() 
-        {
-            new Movie{Id=1,name="Name1"},
-            new Movie{Id=2,name="Name2"},
-        };
+        private readonly ApplicationDbContext _db;
 
-        return View(movies);
+        // Inject the database context
+        public MovieController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // Option 1: Hardcoded movies
+        /*public IActionResult Index()
+        {
+            var movies = new List<Movie>() 
+            {
+                new Movie{Id=1, name="Name1"},
+                new Movie{Id=2, name="Name2"},
+            };
+
+            return View(movies);
+        }*/
+
+        // Option 2: Fetch movies from the database
+        public IActionResult index()
+        {
+            var movies = _db.movies.ToList();
+            return View("Index", movies); // reuse the same view
+        }
     }
-    
 }
--- obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78c9b776937cad286d6e7e5cc04a07bc73b3b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2354f816f60744da3ebb9611b9e59228c53cf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
